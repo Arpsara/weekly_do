76f30035bf5e9f5b791ef8017d0652ae92f98c0f
Arpsara <arpsara@ct2c.fr>
2018-03-15 13:59:49 +0100

    Task - Should validate presence of name

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   belongs_to :project
   has_many :schedules
 
+  validates_presence_of :name
+
   def self.search(search)
     if search
       self.where.has{
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   it { should belong_to :project }
   it { should have_many :schedules }
+
+  it { should validate_presence_of :name }
 end
