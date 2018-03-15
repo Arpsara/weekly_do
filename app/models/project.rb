266feddb5d5006651a7ae8a2a0ac89867bff3a14
Arpsara <arpsara@ct2c.fr>
2018-03-15 14:11:30 +0100

    Project - Validate presence of name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   has_many :tasks
 
+  validates_presence_of :name
+
   def self.search(search)
     if search
       self.where.has{
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it { should have_many :tasks }
+
+  it { should validate_presence_of :name }
 end
