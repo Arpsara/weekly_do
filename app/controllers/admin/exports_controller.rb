cf2584ae97c65e764c27940c3e0a9d7a39aaf9e5
Arpsara <arpsara@ct2c.fr>
2018-03-29 15:41:58 +0200

    Totals in exports

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -4,7 +4,7 @@ class Admin::ExportsController < ApplicationController
   include TimeHelper
 
   def time_entries
-    @time_entries = TimeEntry.where(id: params[:time_entries_ids].split(','))
+    @time_entries = TimeEntry.where(id: JSON.parse(params[:time_entries_ids]).split(','))
 
     authorize :export, :time_entries?
 
@@ -16,11 +16,19 @@ class Admin::ExportsController < ApplicationController
         t('words.date'),
         # "#{I18n.transliterate(TimeEntry.human_attribute_name(:spent_time))} (min)",
         I18n.transliterate(TimeEntry.human_attribute_name(:spent_time)),
-        t('words.price_per_hour'),
-        I18n.transliterate(t('words.cost')),
+        t('words.price_per_hour_ht'),
+        I18n.transliterate(t('words.cost_ht')),
         t('words.user')
       ]
       csv << headers
+
+      total_spent_time = 0
+      total_cost = 0
+
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts @time_entries
+      puts @time_entries.count
+
       @time_entries.each do |time_entry|
         csv << [
           time_entry.id,
@@ -33,7 +41,19 @@ class Admin::ExportsController < ApplicationController
           time_entry.total_cost,
           time_entry.user.fullname
         ]
+
+        total_spent_time += time_entry.spent_time
+        total_cost += time_entry.total_cost
       end
+      csv << [
+        t("words.totals"),
+        "",
+        "",
+        "",
+        readable_time(total_spent_time),
+        "",
+        total_cost
+      ]
     end
     filename = "weeklydo-time-entries-#{Date.today}.csv"
     send_data(csv, filename: filename)
