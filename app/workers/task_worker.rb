f4fd76b3efd0ff54d43998f47e6f9b6d179657c6
Arpsara <arpsara@ct2c.fr>
2018-10-15 16:16:36 +0200

    Update deadline information and task worker

--- app/workers/task_worker.rb
+++ app/workers/task_worker.rb
@@ -1,9 +1,9 @@
 module TaskWorker
   def change_task_priorities
-    Task.where(deadline_date: Date.today + 2.weeks ).each do |task|
+    Task.where.has{(deadline_date >= Date.today + 1.week ) & (deadline_date <= Date.today + 2.weeks )}.each do |task|
       task.update_columns(priority: "high")
     end
-    Task.where(deadline_date: Date.today + 1.week ).each do |task|
+    Task.where.has{(deadline_date >= Date.today ) & (deadline_date < Date.today + 1.week )}.each do |task|
       task.update_columns(priority: "critical")
     end
   end
