635f6f69fdcf001c6aa843993ffd50f682565102
Arpsara <arpsara@ct2c.fr>
2018-12-21 17:59:49 +0100

    Kanban - Fix when date is nil

--- app/controllers/concerns/kanban.rb
+++ app/controllers/concerns/kanban.rb
@@ -4,7 +4,7 @@ module Kanban
   def kanban_variables
     @kanban_states = @project.kanban_states.per_position
     @tasks = @project.tasks.includes(:users)
-    @high_priority_tasks = @tasks.with_high_priority.select{|t| t.kanban_state_id.blank?}.sort_by{|x| x.deadline_date}
-    @todo_tasks = @tasks.without_high_priority.select{|t| t.kanban_state_id.blank?}.sort_by{|x| [x.deadline_date, x.priority]}
+    @high_priority_tasks = @tasks.with_high_priority.select{|t| t.kanban_state_id.blank?}.sort_by{|x| x.deadline_date || Date.today + 99.years}
+    @todo_tasks = @tasks.without_high_priority.select{|t| t.kanban_state_id.blank?}.sort_by{|x| x.deadline_date || Date.today + 99.years }
   end
 end
